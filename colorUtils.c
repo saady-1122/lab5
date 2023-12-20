c7d413486788b4c7e97ac61f320280e3893a085a
saady-1122 <saadkhan1162003@gmail.com>
2023-12-19 22:16:18 -0800

    colorutils.c modified

--- colorUtils.c
+++ colorUtils.c
@@ -1,5 +1,7 @@
 
 #include "colorUtils.h"
+#include <stdio.h>
+#include <math.h>
 
 double rgbIntToFloat(int c) {
   return (c / 255.0);
@@ -10,6 +12,46 @@ int max(int x, int y, int z) {
   m = m > z ? m : z;
   return m;
 }
+
+int min(int x, int y, int z) {
+  int m = x < y ? x : y;
+  m = m < z ? m : z;
+  return m;
+}
+
+int toGrayScaleAverage(int r, int g, int b) {
+  //TODO: test this, it may be wrong!
+  return (r + g + b) / 3;
+}
+
+int toGrayScaleLightness(int r, int g, int b){
+  // lightness technique (max r,g,b + min r,g,b)/2;
+  return (max(r,g,b) + min(r,g,b))/2;
+}
+
+
+int toGrayScaleLuminosity(int r, int g, int b){
+  // luminousty technique 0.21r + 0.72g + 0.07b;
+  return round(0.21*r + 0.72*g + 0.07*b);
+}
+
+// three functions to compute the three sepia-tone RGB values
+int toSepiaRed(int r, int g, int b){
+  // for red 0.393r + 0.769g + 0.189b;
+  return round(0.393*r + 0.769*g + 0.189*b);
+}
+
+int toSepiaGreen(int r, int g, int b){
+  // for green 0.349r + 0.686g + 0.131b;
+  return round(0.349*r + 0.686*g + 0.131*b);
+}
+
+int toSepiaBlue(int r, int g, int b){
+  // for blue 0.272r + 0.534g + 0.131b;
+  return round(0.272*r + 0.534*g + 0.131*b);
+}
+
+
 
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
